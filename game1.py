62394fab2b8f88ad89225a84b183d76f36daf9ec
21ccronk <21ccronk@athenian.org>
2020-09-16 22:38:26 +0000

    Finished guessing game

--- game1.py
+++ game1.py
@@ -1,10 +1,40 @@
-'''
-Use this file to write an "open" version of the game (no test code or defined format). This will be translated into a testable program later. Use the reponse statements provided to make that transition easier.
 
-"Your number is too high."
-"Your number is too low."
-"Your number is out of range."
-"I'm sorry you are giving up!"
-"I'm sorry, you are out of guesses."
+import random
 
-'''
+
+def justright():
+    print("You guessed " + str(y) + ". That is correct!")
+
+def toolow():
+    print("You guessed " + str(y) + ". That is too low!")
+
+def toohigh():
+    print("You guessed " + str(y) + ". That is too high!")
+
+def outofrange():
+    print("You guessed " + str(y) + ". That is out of the range!")
+
+
+x = random.randint(0,10)
+print(x)
+
+for count in range(0,5):
+
+    y = int(input("Try to guess the correct number between 1 and 10: "))
+
+    if y == x:
+        justright()
+        break
+
+    elif y > x:
+        toohigh()
+        if y > 10:
+            outofrange()
+
+    elif y < x:
+        toolow()
+        if y < 0:
+            outofrange()
+
+if count == 4:
+    print("You are out of tries!")
