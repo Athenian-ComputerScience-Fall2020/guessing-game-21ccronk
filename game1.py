6a9b0dc4aa89e4cd565f4b9f24ef3a58f747ffa2
21ccronk <21ccronk@athenian.org>
2020-09-28 01:24:58 +0000

    Unfinished last task

--- game1.py
+++ game1.py
@@ -1,5 +1,6 @@
 # I got help from Megan on this one
 import random
+from tracemalloc import start
 
 
 def justright():
@@ -14,46 +15,55 @@ def toohigh():
 def outofrange():
     print("You guessed " + str(y) + ". That is out of the range!")
 
-print("Hello! Welcome to guessing game! You are about to try to guess a random generated number between your range of choice. What would you like your range to be?")
-r = input("Enter the start of your range: ")
-e = input("Enter the end of your range: ")
+while True:
 
-print("Your range is (" + r + ", " + e + ")")
-gues = int(input("How many guesses would you like: "))
+    print("Hello! Welcome to guessing game! You are about to try to guess a random generated number between your range of choice. What would you like your range to be?")
+    r = input("Enter the start of your range: ")
+    e = input("Enter the end of your range: ")
 
-gues1 = gues - 1
+    print("Your range is (" + r + ", " + e + ")")
+    gues = int(input("How many guesses would you like: "))
 
-x = random.randint(int(r),int(e))
+    gues1 = gues - 1
 
-try:
+    x = random.randint(int(r),int(e))
 
-    for count in range(0, gues):
+    try:
 
-        y = (input("You have " + str(gues) + " tires to guess the correct number between " + r + " and " + e + " or type 'q' to quit: "))
+        for count in range(0, gues):
+
+            y = (input("You have " + str(gues) + " tires to guess the correct number between " + r + " and " + e + " or type 'q' to quit: "))
         
-        if y == "q":
-            quit
+            if y == "q":
+                quit
         
-        else:
-            y = int(y)
+            else:
+                y = int(y)
 
-        if y == x:
-            justright()
-            break
+            if y == x:
+                justright()
+                break
 
-        elif y > x:
-            toohigh()
-            if y > int(e):
-                outofrange()
+            elif y > x:
+                toohigh()
+                if y > int(e):
+                    outofrange()
 
-        elif y < x:
-            toolow()
-            if y < (int(r)):
-                outofrange()
+            elif y < x:
+                toolow()
+                if y < (int(r)):
+                    outofrange()
 
 
-    if count == gues1:
-        print("You are out of tries! " + "The correct number was " + str(x) + ". Thanks for playing!!!")
+        if count == gues1:
+            print("You are out of tries! " + "The correct number was " + str(x) + ". Thanks for playing!!!")
+    
 
-except:
-    print("Please enter a number")
+    except:
+        print("Please enter a number")
+
+    again = input("If you'd like to play again, please type 'yes': ")
+    if again == "yes":
+        start(loop)
+    else:
+        exit
